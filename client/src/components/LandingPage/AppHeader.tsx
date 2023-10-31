4908e0e3dcec28126c9b6c307380166ce4b679c1
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-10-31 17:40:19 -0400

    Added Monkey Logo to Header

--- client/src/components/LandingPage/AppHeader.tsx
+++ client/src/components/LandingPage/AppHeader.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/LandingPage.module.sass";
+import monkeyTextLogo2 from "../../assets/monkeyText2.png";
+import monkeyLogo2 from "../../assets/monkey2.png";
 
 const AppHeader: React.FC = () => {
   return (
     <div className={styles.appHeader}>
-      <div className={styles.logo}>
-        <Link className={styles.link} to="./">
-          logo
-        </Link>
-      </div>
+      {/* <div className={styles.headerLogoContainer}> */}
+      <Link className={styles.headerLogoLink} to="./">
+        <img
+          className={styles.headerLogo}
+          src={monkeyLogo2}
+          alt="Monkey Logo"
+        />
+      </Link>
+      {/* </div> */}
     </div>
   );
 };
