f92138a9707336c802f6737d9cb73d982d4c00fc
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-11-04 19:52:44 -0400

    Prototyped container alignments for Game page UI.

--- client/src/components/GamePage/Game/GameBottomSect.tsx
+++ client/src/components/GamePage/Game/GameBottomSect.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import styles from "./Game.module.sass";
+import BottomVoteBtn from "./bottomVoteBtn";
 
 type Props = {};
 
 const GameBottomSect = (props: Props) => {
   return (
-    <div className="gameBottomSection">
-      gameBottomSection
-      <div className="gameBar">gameBar</div>
-      <div className="votingContainer">votingContainer</div>
+    <div className={styles.gameBottomSect}>
+      <div className={styles.gameBarContainer}>gameBar</div>
+      <div className={styles.votingContainer}>
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+        <BottomVoteBtn />
+      </div>
     </div>
   );
 };
