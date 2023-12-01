e549cd2a524a15aa11ea5909bac5556e72ba5c02
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-12-01 16:56:20 -0500

    ADDED: Gradient Progress bar

--- client/src/components/GamePage/Game/GameBottomSect.tsx
+++ client/src/components/GamePage/Game/GameBottomSect.tsx
@@ -3,24 +3,27 @@ import styles from "./Game.module.sass";
 import BottomVoteBtn from "./BottomVoteBtn";
 import BottomPlaybackBar from "./BottomPlaybackBar";
 import { GameData, RoundContext } from "./Game";
+import { PlaybackProvider } from "./BottomPlaybackBar"; // change later
 
 type GameBottomSectProps = { gameData: React.MutableRefObject<GameData> };
 
 const GameBottomSect = (props: GameBottomSectProps) => {
   return (
-    <div className={styles.gameBottomSect}>
-      <BottomPlaybackBar gameData={props.gameData} />
-      <div className={styles.votingContainer}>
-        <BottomVoteBtn name="Drew" />
-        <BottomVoteBtn name="jesse" />
-        <BottomVoteBtn name="jesse" />
-        <BottomVoteBtn name="jesse" />
-        <BottomVoteBtn />
-        <BottomVoteBtn />
-        <BottomVoteBtn />
-        <BottomVoteBtn />
+    <PlaybackProvider>
+      <div className={styles.gameBottomSect}>
+        <BottomPlaybackBar gameData={props.gameData} />
+        <div className={styles.votingContainer}>
+          <BottomVoteBtn name="Drew" />
+          <BottomVoteBtn name="jesse" />
+          <BottomVoteBtn name="jesse" />
+          <BottomVoteBtn name="jesse" />
+          <BottomVoteBtn />
+          <BottomVoteBtn />
+          <BottomVoteBtn />
+          <BottomVoteBtn />
+        </div>
       </div>
-    </div>
+    </PlaybackProvider>
   );
 };
 
