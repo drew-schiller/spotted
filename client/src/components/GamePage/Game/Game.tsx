e2b7d468b8428f60703c985b133478c88d5e410d
Jesse Gabriel <60901466+wert18784@users.noreply.github.com>
2023-12-01 16:12:15 -0500

    ADDED Audio Playback

--- client/src/components/GamePage/Game/Game.tsx
+++ client/src/components/GamePage/Game/Game.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, useContext, useRef, createContext } from "react";
+import React, {
+  useEffect,
+  useState,
+  useContext,
+  useRef,
+  createContext,
+} from "react";
 import styles from "./Game.module.sass";
 import GameTopSect from "./GameTopSect";
 import GameMidSect from "./GameMidSect";
@@ -6,25 +12,43 @@ import GameBottomSect from "./GameBottomSect";
 import { GamePageUpdateContext } from "../GamePage";
 
 type Props = {};
-export type Image = { url: string, height: number, width: number };
-export type Artist = { id: string, name: string }
-export type Album = { name: string, id: string, images: Array<Image>, artists: Array<Artist> }
-export type Track = { name: string, id: string, album: Album };
-export type GameData = { round_tracks: Array<Track>, rounds: number };
-export const RoundContext = createContext({ round: 0, setRound: (r: number) => { r } });
+export type Image = { url: string; height: number; width: number };
+export type Artist = { id: string; name: string };
+export type Album = {
+  name: string;
+  id: string;
+  images: Array<Image>;
+  artists: Array<Artist>;
+};
+export type Track = {
+  name: string;
+  id: string;
+  album: Album;
+  preview_url: string;
+};
+export type GameData = { round_tracks: Array<Track>; rounds: number };
+export const RoundContext = createContext({
+  round: 0,
+  setRound: (r: number) => {
+    r;
+  },
+});
 
 const Game: React.FC = (_props: Props) => {
   const [minimized, setMinimized] = useState(false);
   const { setGamePageUpdate } = useContext(GamePageUpdateContext);
-  const [ round, setRound ] = useState(0);
+  const [round, setRound] = useState(0);
   const contextValue = { round, setRound };
-  const gameData = useRef<GameData>({ round_tracks: [], rounds: 0});
+  const gameData = useRef<GameData>({ round_tracks: [], rounds: 0 });
 
   useEffect(() => {
     const getGameData = async () => {
       console.log("Fetching game data...");
       try {
-        const response = await fetch('http://127.0.0.1:5000/api/game_data', {credentials: "include", method: "GET"});
+        const response = await fetch("http://127.0.0.1:5000/api/game_data", {
+          credentials: "include",
+          method: "GET",
+        });
         const responseJson = await response.json();
         gameData.current = responseJson;
         setRound(1);
@@ -32,7 +56,7 @@ const Game: React.FC = (_props: Props) => {
         console.error("ERROR: Unable to read game data from session.");
       }
     };
-    
+
     getGameData();
   }, []);
 
@@ -67,9 +91,9 @@ const Game: React.FC = (_props: Props) => {
       <RoundContext.Provider value={contextValue}>
         <GameTopSect gameData={gameData} endGame={endGame} />
         <GameMidSect gameData={gameData} />
-        <GameBottomSect />
+        <GameBottomSect gameData={gameData} />
       </RoundContext.Provider>
-    )
+    );
   };
 
   return (
--- client/src/components/GamePage/Game/PlaybackActionsButton.tsx
+++ client/src/components/GamePage/Game/PlaybackActionsButton.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
 import styles from "./Game.module.sass";
+import { usePlayback } from "./BottomPlaybackBar"; // change later
 
 type Props = {};
 
 const PlaybackActionsButton = (props: Props) => {
+  const { isPlaying, togglePlayback } = usePlayback();
+
+  // Actions button will have multiple states, the default of which being playback
+  // Within the "playback" state, the button can toggle between play and pause.
+  const [buttonState, setButtonState] = React.useState("playback");
+
+  const handleClick = () => {
+    switch (buttonState) {
+      case "playback":
+        togglePlayback();
+        break;
+      case "voting":
+        // Handle click in voting state
+        break;
+      default:
+        break;
+    }
+  };
+
+  const renderButtonIcon = () => {
+    switch (buttonState) {
+      case "playback":
+        return isPlaying ? <FaPause /> : <FaPlay />;
+      case "voting":
+        return "Vote";
+      default:
+        return "";
+    }
+  };
+
   return (
-    <div className={styles.playbackActionsButton}>
-      <FaPlay size="" />
-    </div>
+    <button onClick={handleClick} className={styles.playbackActionsButton}>
+      {renderButtonIcon()}
+    </button>
   );
 };
 
