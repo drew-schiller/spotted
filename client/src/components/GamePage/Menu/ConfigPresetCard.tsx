986a005a1a37bf367dd1a170bcb8a8b975904605
Drew Schiller <drewschille@gmail.com>
2023-12-06 20:50:23 -0500

    Presets now show if selected

--- client/src/components/GamePage/Menu/ConfigPresetCard.tsx
+++ client/src/components/GamePage/Menu/ConfigPresetCard.tsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Menu.module.sass";
 import { Config } from "./Menu";
+import { SelectedPresetContext } from "./ConfigPresetsTab";
 
-type Props = { config: React.MutableRefObject<Config>, title: string; presetIcon: null, item_type: string, gamemode: string };
+type Props = { config: React.MutableRefObject<Config>, id: string; presetIcon: null, item_type: string, gamemode: string };
 
 const ConfigPresetCard = (props: Props) => {
+  const { selected, setSelected } = useContext(SelectedPresetContext);
 
   const selectPreset = () => {
     props.config.current.item_type = props.item_type;
     props.config.current.gamemode = props.gamemode;
+    setSelected(props.id);
   };
 
   return (
-    <button className={styles.configPresetCard} onClick={() => selectPreset()}>
+    <button className={styles.configPresetCard} onClick={() => selectPreset()} style={(selected == props.id) ? {border:"3px solid #8644ca"} : {}}>
       <div className={styles.iconContainer}></div>
       <div className={styles.titleContainer}>
-        <p className={styles.title}>{props.title}</p>
+        <p className={styles.title}>{props.id}</p>
       </div>
     </button>
   );
--- client/src/components/GamePage/Menu/ConfigPresetsTab.tsx
+++ client/src/components/GamePage/Menu/ConfigPresetsTab.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import ConfigPresetCard from "./ConfigPresetCard";
 import styles from "./Menu.module.sass";
 import { Config } from "./Menu";
 
 type Props = { config: React.MutableRefObject<Config> };
+export const SelectedPresetContext = createContext({ selected: "", setSelected: (s: string) => { s }})
 
 const ConfigPresetsTab: React.FC<Props> = (props: Props) => {
+  const [ selected, setSelected ] = useState("classic");
+  const context = { selected, setSelected };
+
   return (
-    <div className={styles.configPresetsTab}>
-      <ConfigPresetCard config={props.config} title="classic" presetIcon={null} item_type="track" gamemode="vote" />
-      <ConfigPresetCard config={props.config} title="album" presetIcon={null} item_type="album" gamemode="vote" />
-      <ConfigPresetCard config={props.config} title="artist" presetIcon={null} item_type="artist" gamemode="vote" />
-    </div>
+    <SelectedPresetContext.Provider value={context}>
+      <div className={styles.configPresetsTab}>
+        <ConfigPresetCard config={props.config} id="classic" presetIcon={null} item_type="track" gamemode="vote" />
+        <ConfigPresetCard config={props.config} id="album" presetIcon={null} item_type="album" gamemode="vote" />
+        <ConfigPresetCard config={props.config} id="artist" presetIcon={null} item_type="artist" gamemode="vote" />
+      </div>
+    </SelectedPresetContext.Provider>
   );
 };
 
